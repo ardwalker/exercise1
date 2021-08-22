b33345e2c54fb0f0bdf19d5408c9dbfb42c0a415
Andrew Walker <ardwalker@protonmail.com>
2021-08-22 17:01:56 +0100

    Scan nothing, an apple and an orange

--- src/main/scala/exercise/Checkout.scala
+++ src/main/scala/exercise/Checkout.scala
@@ -1,5 +1,11 @@
 package exercise
 
 object Checkout {
-  def scan(items: List[String]) = 0
+  def scan(items: List[String]) = {
+    items match {
+      case x :: xs if x eq "Apple"  => 60
+      case x :: xs if x eq "Orange" => 25
+      case _       => 0
+    }
+  }
 }
--- src/test/scala/exercise/CheckoutSpec.scala
+++ src/test/scala/exercise/CheckoutSpec.scala
@@ -4,7 +4,16 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class CheckoutSpec extends AnyFlatSpec with Matchers {
-  "Checkout" should "scan no items" in {
+  "It" should "scan no items" in {
     Checkout.scan(List()) shouldEqual 0
   }
+
+  "It" should "scan an apple" in {
+    Checkout.scan(List("Apple")) shouldEqual 60
+  }
+
+  "It" should "scan an orange" in {
+    Checkout.scan(List("Orange")) shouldEqual 25
+  }
+
 }
